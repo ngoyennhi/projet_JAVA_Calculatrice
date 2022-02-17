995a0e98fef9e8c87534a32d5409a88938cf5ddf
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-02-17 17:28:34 +0100

    mis en place les boutons sur l'interface

--- java_Projet_Calculatrice/src/java_Projet_Calculatrice/CalculatriceSamSar.java
+++ java_Projet_Calculatrice/src/java_Projet_Calculatrice/CalculatriceSamSar.java
@@ -1,6 +1,8 @@
 package java_Projet_Calculatrice;
 
 import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -8,6 +10,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JLabel;
 import net.miginfocom.swing.MigLayout;
+import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.LineBorder;
+import java.awt.Font;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 public class CalculatriceSamSar {
 
@@ -37,27 +49,261 @@ public class CalculatriceSamSar {
 	 */
 	// Constructeur qui est appele dans le main 
 	public CalculatriceSamSar() {
-		initialize();
+//		initialize();
+		calculatriceBtn();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	//Methode initialize qui est appele dans le constructeur
-	private void initialize() {
+//	private void initialize() {
+//		frame = new JFrame();
+//		frame.setBounds(0, 0, 600, 300);
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		
+////		 command + shift + C pour mettre en commentaire les lignes de code
+//		JPanel panel_container = new JPanel();
+//		panel_container.setBackground(Color.PINK);
+//		panel_container.setForeground(Color.BLACK);
+//		frame.getContentPane().add(panel_container, BorderLayout.CENTER);
+//		panel_container.setLayout(null);
+//		
+//		input_title = new JTextField();
+//		input_title.setBounds(267, 124, 76, 26);
+//		panel_container.add(input_title);
+//		input_title.setColumns(10);
+//		
+//		JLabel label_title = new JLabel("Hello SamSar!");
+//		label_title.setBounds(25, 17, 555, 43);
+//		panel_container.add(label_title);
+//		
+//		JButton btnNewButton = new JButton("OK");
+//		btnNewButton.addActionListener(new ActionListener() {
+//			public void actionPerformed(ActionEvent e) {
+//				//On stock le contenu de l'input dans la valeur title qui est de type string avec getText()
+//
+//				  String title = input_title.getText();   
+//				  // On modifie le texte du label dont le nom de variable est label_title avec setTitle()
+//			      label_title.setText(btnNewButton.getText()+ " "+ title);
+//			}
+//			
+//		});
+//		btnNewButton.setBounds(355, 124, 58, 29);
+//		panel_container.add(btnNewButton);
+//	}
+	
+	private void calculatriceBtn() {
 		frame = new JFrame();
-		frame.setBounds(0, 0, 600, 300);
+		frame.getContentPane().setBackground(Color.WHITE);
+		
+		JPanel panel = new JPanel();
+		panel.setForeground(Color.GRAY);
+		panel.setBorder(null);
+		panel.setBackground(Color.DARK_GRAY);
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(21, 27, 303, 80);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("0123456789");
+		lblNewLabel.setBounds(6, 6, 291, 68);
+		panel_1.add(lblNewLabel);
+		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
+		lblNewLabel.setForeground(Color.BLACK);
+		lblNewLabel.setBackground(Color.BLACK);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(6, 150, 338, 327);
+		panel.add(panel_2);
+		
+		JButton btnMRC = new JButton("MRC");
+		
+		JButton btnM_plus = new JButton("M+");
+		
+		JButton btnM_sub = new JButton("M-");
+		
+		JButton btnCE = new JButton("CE");
+		
+		JButton btnON_C = new JButton("ON/C");
+		btnON_C.setForeground(Color.RED);
+		btnON_C.setBackground(Color.RED);
+		
+		JButton btn7 = new JButton("7");
+		
+		JButton btn8 = new JButton("8");
+		
+		JButton btn9 = new JButton("9");
+		
+		JButton btnPercent = new JButton("%");
+		
+		JButton btnSQRT = new JButton("√ A");
+		
+		JButton btn4 = new JButton("4");
+		
+		JButton btn5 = new JButton("5");
+		
+		JButton btn6 = new JButton("6");
+		
+		JButton btnMulti = new JButton("x");
+		
+		JButton btnNewButton_4_1_1 = new JButton("New button");
+		
+		JButton btnPlusSub = new JButton("+/-");
+		btnPlusSub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btn3 = new JButton("3");
+		
+		JButton btn2 = new JButton("2");
+		
+		JButton btn1 = new JButton("1");
+		
+		JButton btn0 = new JButton("0");
+		
+		JButton btnDot = new JButton(".");
+		btnDot.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnDev = new JButton("÷");
+		btnDev.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnEgal = new JButton("=");
+		btnEgal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnSub = new JButton("-");
+		btnSub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		
+		JButton btnPlus = new JButton("+");
+		GroupLayout gl_panel_2 = new GroupLayout(panel_2);
+		gl_panel_2.setHorizontalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_2.createParallelGroup(Alignment.TRAILING)
+							.addGroup(gl_panel_2.createSequentialGroup()
+								.addComponent(btn7, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+								.addGap(6)
+								.addComponent(btn8, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+								.addGap(6)
+								.addComponent(btn9, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+								.addGap(6)
+								.addComponent(btnPercent, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+								.addGap(6)
+								.addComponent(btnSQRT, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+							.addGroup(gl_panel_2.createSequentialGroup()
+								.addComponent(btnMRC, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(btnM_plus, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(btnM_sub, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(btnCE, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+								.addPreferredGap(ComponentPlacement.RELATED)
+								.addComponent(btnON_C, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_panel_2.createSequentialGroup()
+							.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING, false)
+								.addGroup(gl_panel_2.createSequentialGroup()
+									.addComponent(btn4, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+									.addGap(6)
+									.addComponent(btn5, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+									.addGap(6)
+									.addComponent(btn6, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+									.addGap(6)
+									.addComponent(btnMulti, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+								.addGroup(gl_panel_2.createSequentialGroup()
+									.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_panel_2.createSequentialGroup()
+											.addComponent(btn1, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+											.addGap(6)
+											.addComponent(btn2, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+											.addGap(6)
+											.addComponent(btn3, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+										.addGroup(gl_panel_2.createSequentialGroup()
+											.addComponent(btn0, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+											.addGap(6)
+											.addComponent(btnDot, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+											.addGap(6)
+											.addComponent(btnPlusSub, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)))
+									.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addComponent(btnPlus, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel_2.createSequentialGroup()
+									.addComponent(btnDev, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(btnNewButton_4_1_1, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))
+								.addComponent(btnSub, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnEgal, GroupLayout.PREFERRED_SIZE, 60, GroupLayout.PREFERRED_SIZE))))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		gl_panel_2.setVerticalGroup(
+			gl_panel_2.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_2.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.TRAILING, false)
+						.addComponent(btnON_C, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnCE, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnM_sub, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnM_plus, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnMRC, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+						.addComponent(btn7, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btn8, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btn9, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnPercent, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnSQRT, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_2.createSequentialGroup()
+							.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+								.addComponent(btn4, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btn5, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btn6, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnMulti, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnNewButton_4_1_1, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+								.addComponent(btn1, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btn2, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btn3, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+								.addComponent(btn0, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnDot, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+								.addComponent(btnPlusSub, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_panel_2.createSequentialGroup()
+							.addComponent(btnDev, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(gl_panel_2.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_panel_2.createSequentialGroup()
+									.addComponent(btnSub, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE)
+									.addGap(6)
+									.addComponent(btnEgal, GroupLayout.PREFERRED_SIZE, 58, GroupLayout.PREFERRED_SIZE))
+								.addComponent(btnPlus, GroupLayout.PREFERRED_SIZE, 122, GroupLayout.PREFERRED_SIZE))))
+					.addGap(192))
+		);
+		panel_2.setLayout(gl_panel_2);
+		frame.setBounds(10, 10, 350, 550);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel panel_container = new JPanel();
-		panel_container.setBackground(Color.PINK);
-		panel_container.setForeground(Color.BLACK);
-		frame.getContentPane().add(panel_container, BorderLayout.CENTER);
-		panel_container.setLayout(new MigLayout("", "[100][100][100][][100][100][100]", "[50][50][50][50][50][50]"));
-		
-		JLabel label_title = new JLabel("Hello SamSar!");
-		panel_container.add(label_title, "cell 3 3,alignx center,aligny center");
-		
 	}
-
 }
